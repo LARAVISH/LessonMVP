9c2d11ba0a1359b326416e89c282f93c0dbb57af
Lara Vish <fandvor@mail.ru>
2022-08-02 15:20:03 +0500

    сотворили спагетти код

--- app/src/main/java/com/githab/laravish/lessonmvp/MainActivity.kt
+++ app/src/main/java/com/githab/laravish/lessonmvp/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.githab.laravish.lessonmvp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.githab.laravish.lessonmvp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val counters = mutableListOf(0, 0, 0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+
+        with(binding) {
+            btnOne.setOnClickListener { textViewOne.text = "${++counters[0]}" }
+            btnTwo.setOnClickListener { textViewTwo.text = "${++counters[1]}" }
+            btnThree.setOnClickListener { textViewThree.text = "${++counters[2]}" }
+        }
+    }
+
+    private fun initView() = with(binding) {
+        textViewOne.text = "${counters[0]}"
+        textViewTwo.text = "${counters[1]}"
+        textViewThree.text = "${counters[2]}"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray("KEY", counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val array = savedInstanceState.getIntArray("KEY")
+        counters.let { list ->
+            list.clear()
+            array?.toList()?.let {
+                list.addAll(it)
+            }
+        }
+        initView()
     }
 }
