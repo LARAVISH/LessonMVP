4689007bdaf3baad5350e404b1f5183f72d232d7
Lara Vish <fandvor@mail.ru>
2022-08-02 08:40:01 +0500

    Initial new project

---
+++ app/src/main/java/com/githab/laravish/lessonmvp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.githab.laravish.lessonmvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
